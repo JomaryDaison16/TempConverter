37f3502979c4cdbef717e4775db79e82720e715a
anon1303 <jomari.daison@gmail.com>
2021-12-02 14:08:02 +0800

    updated README.md

--- TempConverter.py
+++ TempConverter.py
@@ -1,17 +1,5 @@
-# # TEMPERATURE CONVERTER
-# # PYTHON PROGRAM THAT CONVERTS 
-# # TEMPERATURE FROM CELSIUS TO FAHRENHEIT
-# # FAHRENHEIT = 9/5*C + 32
 
 c = float(input("Enter Temperature in CELSIUS: "))
 f = 9/5*c + 32 
 print(f,"C")
 
-
-# # Python program that converts mins and seconds into hr
-# # Hr = (min/60) + (sec/3600)
-
-# m = int(input("Enter Min: "))
-# s = int(input("Enter Sec: "))
-# hr = (m/60)+(s/3600)
-# print(hr,"hr")
